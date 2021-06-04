10c76d0a1503e197fdface67f4ea2b121cd91141
gimhyeonjae <hyunjae1341@gmail.com>
2021-06-04 16:45:00 +0900

    TypeScript - typescript - chrome redux devtools 연동

--- react-ts/src/index.tsx
+++ react-ts/src/index.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppStart from './AppStart';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer);
+// chrome redux devtools 사용
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
+  }
+}
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(
   <Provider store={store}>
